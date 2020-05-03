cc7fd6fcb48b65c830deb7bd5610844da06dc558
vitebo <vitebo@hotmail.com>
2020-05-03 18:29:22 -0300

    feat(appointments)

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+    this.appointments.push(appointment);
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    return (
+      this.appointments.find(appointment => isEqual(date, appointment.date)) ||
+      null
+    );
+  }
+}
+
+export default AppointmentsRepository;
